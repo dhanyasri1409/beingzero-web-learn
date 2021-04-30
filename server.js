e863e1848e99a58d65973a3f69f0f5e4aa4f4515
dhanyasri1409 <dhanyasritamada@gmail.com>
2021-04-30 10:58:59 +0530

    my resume

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
  
 const app = express();
-app.use(express.static(__dirname+"\frontend"));
+app.use(express.static(__dirname+"/frontend"));
  
 app.get("/", function(req, res){
     res.send("Welcome to Dhanya's Site ");
