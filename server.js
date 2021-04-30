529adad32af32eeea2f60f91b9b2a391f9ec4e1c
dhanyasri1409 <dhanyasritamada@gmail.com>
2021-04-30 10:41:25 +0530

    my full resume

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
  
 const app = express();
+app.use(express.static(__dirname+"\frontend"));
  
 app.get("/", function(req, res){
     res.send("Welcome to Dhanya's Site ");
 });
 app.get("/resume", function(req, res){
     
-    let file= __dirname+"/myresume.html";
+    let file= __dirname+"/frontend/html/resume.html";
     res.sendFile(file);
 
 })
