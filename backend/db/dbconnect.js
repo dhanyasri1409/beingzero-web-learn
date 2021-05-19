8130cc61803b8030a2ee572cbc6732b7bc4091ac
dhanyasri1409 <dhanyasritamada@gmail.com>
2021-05-19 10:26:59 +0530

    n

--- backend/db/dbconnect.js
+++ backend/db/dbconnect.js
@@ -4,7 +4,7 @@ module.exports.connect=function(){
     var password = process.env.Mongo_atlas_password;
     var connectionString = "mongodb+srv://dhanyasrit:"+password+"@cluster0.v4dsq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-mongoose.connect(connectionString, {});
+mongoose.connect(connectionString, {useUnifiedTopology: true,useNewUrlParser: true});
 mongoose.connection.on('connected', function(){
     console.log("Database connected");
 
